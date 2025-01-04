f366077d590de842347fe97b7ff6e578dc397f2a
zxecsm <zxecsm@outlook.com>
2025-01-04 13:12:09 +0800

    backup

--- server/data/config.js
+++ server/data/config.js
@@ -1,7 +1,8 @@
 import os from 'os';
 
-const filepath = `${os.homedir()}/hello`; // 网站数据存放目录
-const rootP = os.homedir(); // 文件管理器根目录
+const userHome = os.homedir().replace(/\\/g, '/'); // 系统用户家目录
+const filepath = `${userHome}/hello`; // 网站数据存放目录
+const rootP = userHome; // 文件管理器根目录
 const configObj = {
   port: 55555,
   filepath,
