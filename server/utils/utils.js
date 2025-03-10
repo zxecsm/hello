cd2020253a81d70e8d5730f0f278a4c7557aea9c
zxecsm <zxecsm@outlook.com>
2025-03-10 12:46:08 +0800

    backup

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -518,32 +518,21 @@ export function replaceObjectValue(obj, msg) {
 }
 
 // 深拷贝
-export function deepClone(obj) {
-  //判断传入对象为数组或者对象
-  const result = Array.isArray(obj) ? [] : {};
-  // for in遍历
-  for (let key in obj) {
-    // 判断是否为自身的属性值（排除原型链干扰）
-    if (obj.hasOwnProperty(key)) {
-      // 判断对象的属性值中存储的数据类型是否为对象
-      if (typeof obj[key] === 'object') {
-        // 有可能等于null
-        if (obj[key] === null) {
-          result[key] = null;
-          continue;
-        }
+export function deepClone(obj, hash = new WeakMap()) {
+  if (obj === null || typeof obj !== 'object') return obj;
 
-        // 递归调用
-        result[key] = deepClone(obj[key]); //递归复制
-      }
-      // 不是的话直接赋值
-      else {
-        result[key] = obj[key];
-      }
+  if (hash.has(obj)) return hash.get(obj);
+
+  const clone = Array.isArray(obj) ? [] : {};
+  hash.set(obj, clone);
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clone[key] = deepClone(obj[key], hash);
     }
   }
-  // 返回新的对象
-  return result;
+
+  return clone;
 }
 
 // 歌曲标签信息
